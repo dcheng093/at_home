043787418992e1c56c828fda5dafa7df06b1eca6
dcheng <dpeter093@gmail.com>
2025-08-24 03:29:33 +0800

    poop

---
+++ 42/piscine/C06/ex03/ft_sort_params.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcheng <dcheng@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 03:12:13 by dcheng            #+#    #+#             */
+/*   Updated: 2025/08/24 03:12:13 by dcheng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 0;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write (1, &argv[i][j++], 1);
+		}
+		write (1, "\n", 1);
+		i++;
+	}
+}
